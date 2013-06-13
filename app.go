10571dbda14496c028c11d64f96758528d190218
Erik Wong <k4orta@gmail.com>
2013-06-12 22:18:11 -0700

    Added args and a copy file function

--- app.go
+++ app.go
@@ -2,27 +2,39 @@ package main
 
 import (
 	"github.com/howeyc/fsnotify"
+	"io"
 	"log"
+	"os"
+	"path"
 )
 
 func main() {
-	// fmt.Println("Hello World!")
+	watchDir := os.Args[1]
+	moveDir := os.Args[2]
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	done := make(chan bool)
-
+	log.Println("File Spy is running...")
 	go func() {
 		for {
 			select {
 			case ev := <-watcher.Event:
-				log.Println("event: ", ev)
+				if ev.IsCreate() || ev.IsModify() {
+					log.Println("Moving a file...")
+					_, filename := path.Split(ev.Name)
+					efv := copyFile(ev.Name, moveDir+"/"+filename)
+					if efv != nil {
+						log.Println(efv)
+					}
+				}
+
 			}
 		}
 	}()
-	err = watcher.Watch("testDir")
+	err = watcher.Watch(watchDir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,4 +42,23 @@ func main() {
 	<-done
 
 	watcher.Close()
+}
+
+func copyFile(filein string, fileout string) (err error) {
+	src, err := os.Open(filein)
+	defer src.Close()
+	dest, err := os.Create(fileout)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+	_, err = io.Copy(dest, src)
+	if err == nil {
+		si, err := os.Stat(filein)
+		if err != nil {
+			err = os.Chmod(fileout, si.Mode())
+		}
+	}
+
+	return
 }
